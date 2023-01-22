66e00780a272ffaa43a65e9df9d2c9442ba76fc9
gguilioti <giovanni.guilioti@gmail.com>
2023-01-22 20:10:13 -0300

    fix side menu EditarPerfil

--- e-manga/src/pages/EditarPerfil.vue
+++ e-manga/src/pages/EditarPerfil.vue
@@ -1,32 +1,7 @@
 <template>
   <q-layout view="lHh Lpr lFf">
     <q-header elevated>
-      <q-toolbar class="bg-primary">
-        <q-btn
-          flat
-          dense
-          round
-          icon="menu"
-          aria-label="Menu"
-          @click="toggleLeftDrawer"
-        />
-
-        <q-toolbar-title class="bg-primary">
-          Editar Perfil
-        </q-toolbar-title>
-        <div>
-          <q-btn flat color="white" label="Entrar/Cadastrar"
-          size="13px"
-          />
-        </div>
-        <div>
-          <q-btn round>
-            <q-avatar size="42px">
-              <img src="public/avatar.png">
-            </q-avatar>
-          </q-btn>
-        </div>
-      </q-toolbar>
+      <ToolbarMenu @leftDrawer="toggleLeftDrawer"/>
     </q-header>
 
     <q-page-container class="flex justify-center q-pa-md" style="display: grid; justify-content: center; padding-top:100px;">
@@ -107,9 +82,6 @@
         </q-item-label>
 
         <EssentialLink
-          v-for="link in essentialLinks"
-          :key="link.title"
-          v-bind="link"
         />
 
       </q-list>
@@ -119,58 +91,20 @@
 
 <script>
 import { defineComponent, ref } from 'vue'
+import ToolbarMenu from 'components/ToolbarMenu.vue'
 import EssentialLink from 'components/EssentialLink.vue'
-const linksList = [
-  {
-    title: 'Início',
-    icon: 'school',
-    link: 'https://quasar.dev'
-  },
-  {
-    title: 'Pedidos',
-    icon: 'code',
-    link: 'https://github.com/quasarframework'
-  },
-  {
-    title: 'Criar anúncio',
-    icon: 'chat',
-    link: 'https://chat.quasar.dev'
-  },
-  {
-    title: 'Minhas vendas',
-    icon: 'record_voice_over',
-    link: 'https://forum.quasar.dev'
-  },
-  {
-    title: 'Meus produtos',
-    icon: 'rss_feed',
-    link: 'https://twitter.quasar.dev'
-  },
-  {
-    title: 'Carrinho',
-    icon: 'public',
-    link: 'https://facebook.quasar.dev'
-  },
-  {
-    title: 'Meu perfil',
-    icon: 'favorite',
-    link: 'https://awesome.quasar.dev'
-  },
-  {
-    title: 'Pagamentos',
-    icon: 'receipt',
-    link: 'localhost:9000/#/'
-  }
-]
+
 export default defineComponent({
   name: 'MainLayout',
+
   components: {
-    EssentialLink
+    EssentialLink,
+    ToolbarMenu
   },
+
   setup () {
     const leftDrawerOpen = ref(false)
     return {
-      essentialLinks: linksList,
       leftDrawerOpen,
       toggleLeftDrawer () {
         leftDrawerOpen.value = !leftDrawerOpen.value
